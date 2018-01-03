ed64bd6141b0730b8d36c9b2deee08c82f500189
Jerry W <jerrywiltse@gmail.com>
2018-01-03 09:54:07 -0500

    initial commit of new repo for catch2

---
+++ conanfile.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, tools
+import os
+
+
+class Catch2Conan(ConanFile):
+    name = "catch2"
+    version = "2.0.1"
+    description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
+    url = "https://github.com/bincrafters/conan-catch"
+    license = "BSL-1.0"
+    exports = ["LICENSE.md"]
+    source_subfolder = "source_subfolder"
+    header_name = "catch.hpp"
+
+    def source(self):
+        source_url = "https://github.com/catchorg/Catch2"
+        
+        tools.download(
+            "{0}/releases/download/v{1}/{2}".format(source_url, self.version, self.header_name),
+            self.header_name
+        )
+        
+        # All this to get the LICENSE.txt
+        extracted_dir = "Catch2-" + self.version
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        os.rename(extracted_dir, self.source_subfolder)
+        
+        
+    def package(self):
+        self.copy(pattern="LICENSE.txt", src=self.source_subfolder)
+        self.copy(pattern=self.header_name, dst="include")
+
+    def package_id(self):
+        self.info.header_only()
+        
---
+++ test_package/test_package.cpp
@@ -0,0 +1,6 @@
+ #define CATCH_CONFIG_MAIN
+ #include "catch.hpp"
+
+TEST_CASE( "test_package", "[test]" ) {
+	REQUIRE(1==1);
+}
