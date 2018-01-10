260dab5d6a82475e7aaf1eba470f39c1e0f5f610
Uilian Ries <uilianries@gmail.com>
2018-01-10 13:03:50 -0200

    Bump version to 2.1.0
    - Update Catch 2 to version 2.1.0
    - Added Bincrafters as author
    - Added url homepage
    - Moved license file to "licenses" dir
    - Update license path on README file
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,32 +7,33 @@ import os
 
 class Catch2Conan(ConanFile):
     name = "catch2"
-    version = "2.0.1"
+    version = "2.1.0"
     description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
+    homepage = "https://github.com/catchorg/Catch2"
     url = "https://github.com/bincrafters/conan-catch"
-    license = "BSL-1.0"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "https://github.com/catchorg/Catch2/blob/master/LICENSE.txt"
     exports = ["LICENSE.md"]
     source_subfolder = "source_subfolder"
     header_name = "catch.hpp"
 
     def source(self):
         source_url = "https://github.com/catchorg/Catch2"
-        
+
         tools.download(
             "{0}/releases/download/v{1}/{2}".format(source_url, self.version, self.header_name),
             self.header_name
         )
-        
+
         # All this to get the LICENSE.txt
         extracted_dir = "Catch2-" + self.version
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         os.rename(extracted_dir, self.source_subfolder)
-        
-        
+
+
     def package(self):
-        self.copy(pattern="LICENSE.txt", src=self.source_subfolder)
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
         self.copy(pattern=self.header_name, dst="include")
 
     def package_id(self):
         self.info.header_only()
-        
