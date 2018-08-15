3d4ef0d005f6b12e0655dc0043a9ee4ecd706976
Uilian Ries <uilianries@gmail.com>
2018-08-15 08:56:06 -0300

    #397 Install Catch2 cmake files
    - Add ConfigCatch2.cmake
    - Install all CMake stuff in package folder
    - Update CONAN_CMAKE_MODULE_PATH to package root path
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import ConanFile, tools, CMake
 import os
+import tempfile
 
 
 class Catch2Conan(ConanFile):
@@ -16,6 +17,8 @@ class Catch2Conan(ConanFile):
     exports = ["LICENSE.md"]
     source_subfolder = "source_subfolder"
     header_name = "catch.hpp"
+    generators = "cmake"
+    exports_sources = "CMakeLists.txt"
     no_copy_source = True
 
     def source(self):
@@ -31,9 +34,14 @@ class Catch2Conan(ConanFile):
         os.rename(extracted_dir, self.source_subfolder)
 
     def package(self):
+        install_dir = tempfile.mkdtemp()
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = install_dir
+        cmake.configure()
+        cmake.install()
         self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
         self.copy(pattern=self.header_name, dst=os.path.join("include", self.name))
-        self.copy(pattern="*.cmake", dst="res", src=os.path.join(self.source_subfolder, "contrib"))
+        self.copy(pattern="*.cmake", dst=".", src=os.path.join(install_dir, "lib", "cmake", "Catch2"))
 
     def package_id(self):
         self.info.header_only()
