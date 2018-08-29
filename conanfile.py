1151cdee36c47cb01b221c278fa0e59748287889
Uilian Ries <uilianries@gmail.com>
2018-08-29 00:11:15 -0300

    Avoid to check target arch
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -44,10 +44,7 @@ class Catch2Conan(ConanFile):
         self.copy(pattern=self.header_name, dst=os.path.join("include", self.name))
         self.copy(pattern="*.cmake", dst=module_dir, src=os.path.join(install_dir, module_dir))
         tools.replace_in_file(os.path.join(self.package_folder, module_dir, "Catch2ConfigVersion.cmake"),
-         "# if the installed or the using project don't have CMAKE_SIZEOF_VOID_P set, ignore it:", """if ("${CMAKE_GENERATOR}" MATCHES "(Win64|IA64)")
-            return()
-         endif()
-         """)
+         "# if the installed or the using project don't have CMAKE_SIZEOF_VOID_P set, ignore it:", "return()")
 
     def package_id(self):
         self.info.header_only()
