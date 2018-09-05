764ed4d1634e8ef4a5f4d6bec7e067d68ccd76b2
Uilian Ries <uilianries@gmail.com>
2018-09-05 20:39:48 -0300

    Bump minor version to 2.4.0
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import tempfile
 
 class Catch2Conan(ConanFile):
     name = "catch2"
-    version = "2.3.0"
+    version = "2.4.0"
     description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
     homepage = "https://github.com/catchorg/Catch2"
     url = "https://github.com/bincrafters/conan-catch"
@@ -22,23 +22,25 @@ class Catch2Conan(ConanFile):
     no_copy_source = True
 
     def source(self):
-        source_url = "https://github.com/catchorg/Catch2"
         tools.download(
-            "{0}/releases/download/v{1}/{2}".format(source_url, self.version, self.header_name),
+            "{0}/releases/download/v{1}/{2}".format(self.homepage, self.version, self.header_name),
             self.header_name
         )
 
         # All this to get the LICENSE.txt
         extracted_dir = "Catch2-" + self.version
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         os.rename(extracted_dir, self.source_subfolder)
 
     def package(self):
         install_dir = tempfile.mkdtemp()
         module_dir = os.path.join("lib", "cmake", "Catch2")
         cmake = CMake(self)
+        cmake.definitions["CATCH_BUILD_TESTING"] = False
+        cmake.definitions["CATCH_INSTALL_DOCS"] = False
         cmake.definitions["CMAKE_INSTALL_PREFIX"] = install_dir
         cmake.definitions["CMAKE_INSTALL_LIBDIR"] = "lib"
+        cmake.definitions["NOT_SUBPROJECT"] = True
         cmake.configure()
         cmake.install()
         self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
