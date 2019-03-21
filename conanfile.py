c69732ba50c5eded6a7170e669b58359f4d43ae8
Uilian Ries <uilianries@gmail.com>
2019-03-21 15:15:39 -0300

    Bump version to 2.7.0

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import tempfile
 
 class Catch2Conan(ConanFile):
     name = "catch2"
-    version = "2.5.0"
+    version = "2.7.0"
     description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
     homepage = "https://github.com/catchorg/Catch2"
     url = "https://github.com/bincrafters/conan-catch"
